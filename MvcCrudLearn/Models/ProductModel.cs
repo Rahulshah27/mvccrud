f49dbf64e016c635ccfc622d6af0602ef506e27a
Rahul Shah <rahulshah0210@gmail.com>
2021-10-15 02:12:48 +0530

    init-commit

---
+++ MvcCrudLearn/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MvcCrudLearn.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+
+    }
+}
